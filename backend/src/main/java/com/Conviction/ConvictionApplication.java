5acf4e467d74d79fa0aece6801842bf0b2109e46
michaelgiovannisie <siemichaelgiovanni@gmail.com>
2026-06-03 08:25:16 -0400

    add a lot

---
+++ backend/src/main/java/com/Conviction/ConvictionApplication.java
@@ -0,0 +1,13 @@
+package com.Conviction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvictionApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/Conviction/ConvictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Conviction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
