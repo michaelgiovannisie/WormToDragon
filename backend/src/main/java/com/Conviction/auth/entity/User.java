b05dc4bc792d4263ffbf89737af1cb351687a191
michaelgiovannisie <siemichaelgiovanni@gmail.com>
2026-06-03 09:43:55 -0400

    userupdate

--- backend/src/main/java/com/Conviction/auth/entity/User.java
+++ backend/src/main/java/com/Conviction/auth/entity/User.java
@@ -11,7 +11,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
