8c21ba5a865ae78273cf826fad318e43dc6919f0
michaelgiovannisie <siemichaelgiovanni@gmail.com>
2026-06-04 20:30:27 -0400

    postgresql

---
+++ backend/src/main/java/com/Conviction/imports/controller/ImportController.java
@@ -0,0 +1,33 @@
+package com.conviction.imports.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.conviction.imports.service.RobinhoodImportService;
+
+@RestController
+@RequestMapping("/api/import")
+public class ImportController {
+
+    private final RobinhoodImportService importService;
+
+    public ImportController(
+            RobinhoodImportService importService
+    ) {
+        this.importService = importService;
+    }
+
+    @PostMapping("/robinhood")
+    public ResponseEntity<String> importRobinhood(
+            @RequestParam("file")
+            MultipartFile file
+    ) {
+
+        importService.importCsv(file);
+
+        return ResponseEntity.ok(
+                "Robinhood CSV imported successfully"
+        );
+    }
+}
---
+++ backend/src/main/java/com/Conviction/imports/service/RobinhoodImportService.java
@@ -0,0 +1,14 @@
+package com.conviction.imports.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class RobinhoodImportService {
+
+    public void importCsv(
+            MultipartFile file
+    ) {
+
+    }
+}
