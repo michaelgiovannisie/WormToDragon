59578047bdd6148dff2d5d2f9f0e001f9f495ab7
michaelgiovannisie <siemichaelgiovanni@gmail.com>
2026-06-07 19:49:29 -0400

    debug

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
 export const API = "http://localhost:8080/api";
 
-export const PORTFOLIO_ID = "a32e04b9-9270-45d2-a16f-34b6389dc464";
-export const ACCOUNT_ID   = "f5afd398-4c6d-4fa7-8493-5031bc7d27ec";
+export const PORTFOLIO_ID = "cd814fb9-0496-43b9-9f0a-632f5bafbe67";
+export const ACCOUNT_ID   = "07441db5-e021-4ddf-991a-94c7a9ddc500";
