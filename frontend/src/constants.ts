df8d4d54d3112a56efb3771378f0b88631c4fd1b
Michael Sie <siemichaelgiovanni@gmail.com>
2026-08-09 16:00:18 -0400

    update

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
-export const API = "http://localhost:8080/api";
+export const API = import.meta.env.VITE_API_URL ?? "http://localhost:8080/api";
 
 export const PORTFOLIO_ID = "4bed7a37-de64-4b4f-9fda-bcb8bfc10786";
 export const ACCOUNT_ID   = "ff3dad36-62c2-4126-bfca-34fc3cabdcf8";
